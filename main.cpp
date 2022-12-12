29c9e8b15771894b7c95fbb3d8fb29484460afa2
GioFeeera <117982966+GioFeeera@users.noreply.github.com>
2022-12-12 15:36:32 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+float x=0;
+int main() {
+    int n=0;
+    cin >>n;
+    float numero [n];
+    for (int i = 0; i < n; i++) {
+        cin >> numero [i];
+    }
+    for (int i = 0; i < n; ++i) {
+        float mag= maggiore(numero[i], numero[i-1]);
+        if (n>x){
+            x=n;
+        }
+        cout << x;
+    }
+
+    return 0;
 }
